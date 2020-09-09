1ab165bae7c7897b5bc7356eaeee02689da70f01
Eli Huebner <elimhuebner@gmail.com>
2020-09-09 23:24:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   collected_collection = []
   while i < collection.length
-    yield collection[i]
+    collected_collection << yield collection[i]
     i += 1
   end
   collected_collection
